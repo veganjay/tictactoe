9b4f24a2b8d6da93d3ab3b7b837b6a6f657ecfd5
Jason <veganjay@gmail.com>
2012-11-11 10:59:34 -0500

    Choose start player and play again
    Add choices to ask if user want to be start player, and whether the user
    want to play again.  Instead of using Scanner, use BufferedInputReader
    so the return key can be used for default choices.

--- src/com/veganjay/tictactoe/TicTacToe.java
+++ src/com/veganjay/tictactoe/TicTacToe.java
@@ -1,6 +1,8 @@
 package com.veganjay.tictactoe;
 
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 import com.veganjay.tictactoe.TicTacToeBoard.Piece;
 
@@ -11,23 +13,26 @@ import com.veganjay.tictactoe.TicTacToeBoard.Piece;
  * @author jason
  * 
  * TODO
- * - Choose Start Player
- * - Play again
- * - Clean up code
+ * - Clean up code - main loop
+ * - Set better objective value in AI
  * - Graphical version
  */
 public class TicTacToe {
 	public static final Piece START_PLAYER = Piece.X;
+	public static final Piece NEXT_PLAYER  = Piece.O;
 	
 	// Member variables
-	private Scanner input;
+	private BufferedReader input;
 	private TicTacToeBoard board;
 	
 	private Piece currentPlayer;
-	private Piece humanPiece = START_PLAYER;
+
+	private Piece computer;
+	private Piece human;
 	
 	public TicTacToe() {
-		this.input=new Scanner(System.in);	
+		this.input= new BufferedReader(new InputStreamReader(System.in));
+
 		this.board = new TicTacToeBoard();
 	}
 	
@@ -36,7 +41,7 @@ public class TicTacToe {
 		int    col;
 				
 		// Read from input
-		choice = input.next();
+		choice = this.getInput();
 		System.out.println();
 		
 		try {
@@ -48,23 +53,75 @@ public class TicTacToe {
 		return col;
 	}
 	
-	public void mainLoop() {
+	private void getStartPlayer() {
+		String choice;
+		System.out.print("Do you want to go first? [y] ");
+		
+		choice = this.getInput();
+		
+		if (choice.equalsIgnoreCase("n") || choice.equalsIgnoreCase("no")) {
+			this.computer = START_PLAYER;
+			this.human    = NEXT_PLAYER;
+		} else {
+			this.human = START_PLAYER;
+			this.computer = NEXT_PLAYER;
+		}		
+	}
+	
+	private boolean playAgain() {
+		boolean playAgain = true;
+		System.out.print("Do you want to play again? [y] ");
+		String choice = this.getInput();
+		if (choice.equalsIgnoreCase("n") || choice.equalsIgnoreCase("no")) {
+			playAgain = false;
+		}
+		return playAgain;
+	}
+	
+	public String getInput() {
+		String line = "";
+		try {
+			return this.input.readLine();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return line;
+		
+	}
+	
+	public void play() {
 		// Initialize with start player
 		currentPlayer = START_PLAYER;
-		TicTacToeAI ai = new TicTacToeAI(Piece.O);
 		
+		// Determine the start player
+		this.getStartPlayer();
+
+		// Create computer AI with
+		TicTacToeAI ai = new TicTacToeAI(computer);
+				
 		// Display the board;
 		board.printBoard();
 		
+		// Enter main loop
+		mainLoop(ai);
+
+		if (playAgain()) {
+			// Reset the board
+			board.initialize();
+			play();
+		}
+		
+	} // End main loop
+
+	private void mainLoop(TicTacToeAI ai) {
 		while (true) {		
 			int spaceNum;
-			if (currentPlayer == humanPiece) {
+			if (currentPlayer == human) {
 				// Get the choice
 				System.out.print("Enter Space Number (1-9): ");
 				spaceNum = getChoice();							
 			} else {
 				spaceNum = ai.getMove(board); 
-				//spaceNum = ai.getComputerMove(board);
 				System.out.println("Computer moves to " + spaceNum);
 			}
 
@@ -90,8 +147,7 @@ public class TicTacToe {
 			this.switchPlayers();
 			
 		} // End while
-
-	} // End main loop
+	}
 	
 	private void switchPlayers() {
 		if (currentPlayer == Piece.X) {
@@ -102,6 +158,6 @@ public class TicTacToe {
 	}
 	public static void main (String [] args) {
 		TicTacToe game = new TicTacToe();
-		game.mainLoop();
+		game.play();
 	}
 }
